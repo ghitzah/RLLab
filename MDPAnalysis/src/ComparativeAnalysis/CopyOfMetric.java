322102d56f2a255389a8fb0f0c2123beccde1d7e
Gheorghe Comanici <ghitzah@gmail.com>
2014-09-15 14:19:51 -0400

    might be some problem...check with Sherry

---
+++ MDPAnalysis/src/ComparativeAnalysis/CopyOfMetric.java
@@ -0,0 +1,65 @@
+package ComparativeAnalysis;
+
+import java.lang.Exception;
+
+public class CopyOfMetric {
+	
+	/**
+	 * dists: contains the distance values, but only the lower triangular part of the
+	 * distance matrix
+	 */
+	private double[] dists;
+	private int size;
+	
+	public CopyOfMetric(int size) throws OOBException{
+		this.size = size;
+		if (size <= 0 ) throw new OOBException("Size is " + size); 
+		dists = new double[size * (size-1) / 2];
+		for (int i = 0; i < dists.length; i++) {
+			dists[i] = 0.0;
+		}
+	} 
+	
+	public double dist(int s1, int s2) throws OOBException{
+		if (!check_bounds(s1, s2)) throw new OOBException("States are " + s1 +  " "  + s2);
+		if ( s1 == s2) return 0;
+		if( s1 < s2 ) { int tmp = s1; s1 = s2; s2 = tmp; } //swap
+		return dists[(s1-1)*s1/2 + s2];
+	} //DONE
+	
+	public void set(int s1, int s2, double new_val) throws OOBException {
+		if (!check_bounds(s1, s2) || (s1 == s2 && new_val != 0)) throw new OOBException("States are " + s1 +  " "  + s2);
+		if( s1 < s2 ) { int tmp = s1; s1 = s2; s2 = tmp; } //swap
+		dists[(s1-1)*s1/2 + s2] = new_val;
+	}
+	
+	@SuppressWarnings("serial")
+	public class OOBException extends Exception {
+		String s;
+		public OOBException(String s) {
+			this.s = s;
+		}
+		
+		public void printS() { System.out.println(s);}
+	}
+	
+	
+	
+	
+	private boolean check_bounds(int s1, int s2) {
+		if (s1 < 0 || s2 < 0 || s1 >= size || s2 >= size) return false;
+		else return true;
+	}
+	
+	
+	@Override
+	public String toString() {
+		String ss = "";
+		for (int i = 1; i < size; i++) {
+			for (int j = 0; j < i-1; j++) {
+				ss += dists[(i-1)*i/2 + j] + " ";
+			}ss += dists[(i-1)*i/2 + i-1] + "\n";
+		}
+		return ss;
+	}
+}
