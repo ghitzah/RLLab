05fd91b1a78a9a8bdbfd46c762dbcfebf0a89569
Sherry Ruan <ruanshanshansc@gmail.com>
2015-01-06 18:32:36 -0500

    dealt with the first layear, works now

--- Deep/DeepBisim/src/CompGraphs/NewNewAlgo.java
+++ Deep/DeepBisim/src/CompGraphs/NewNewAlgo.java
@@ -12,9 +12,12 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import CompGraphs.DeclustGraph.AlgorithmicException;
+import CompGraphs.Graph.Node;
 import CompGraphs.ModelComparator.IncorrectModelExpection;
+import CompGraphs.NewAlgoSimple.MemoizedFeature;
 import MDP.MDP;
 import MDP.MDP.Action;
+import MDP.MDP.ExactStateModel;
 import MDP.MDP.Feature;
 import MDP.MDP.Measure;
 import MDP.MDP.Model;
@@ -38,7 +41,8 @@ public class NewNewAlgo extends Graph{
 		super(m);
 		this.numIters = numIters;
 		try {
-			addNewLayer();
+			
+			addNewLayer(true);
 		} catch (AlgorithmicException e) {
 			e.printStackTrace();
 		}	
@@ -49,110 +53,179 @@ public class NewNewAlgo extends Graph{
 	}
 
 
+	public void addNewLayer() throws AlgorithmicException{
+		addNewLayer(false);
+	}
 
 
+	private void addNewLayer(boolean firstLayer) throws AlgorithmicException {
+		if(firstLayer){
+			// this will maintain the index of new nodes
+			int nextIndex = (allNodes == null) ? 0 : allNodes.size();
 
-	public void addNewLayer() throws AlgorithmicException {
-		// this will maintain the index of new nodes
-		int nextIndex = (allNodes == null) ? 0 : allNodes.size();
+			// save the last layer from old graph - needed to test transition map
+			Set<Node> prevLayer = finalLayer;
 
-		// save the last layer from old graph - needed to test transition map
-		Set<Node> prevLayer = finalLayer;
+			//we will rebuild the final layer from 0
+			finalLayer = new TreeSet<Node>();
 
-		//we will rebuild the final layer from 0
-		finalLayer = new TreeSet<Node>();
+			// get a fixed number of states to determine which nodes to create
+			Iterator<State> ada = m.get_state_iterator();
+			int tmpNumIters = numIters;
 
-		// get a fixed number of states to determine which nodes to create
-		Iterator<State> ada = m.get_state_iterator();
-		int tmpNumIters = numIters;
-		
-		// heuristic used to select a subset of nodes to analyze
-		Heuristic myHeuristic = new Heuristic(100, Heuristic.HeuristicType.RANDOM_PAIR);
-
-		// select a subset of nodes to analyze
-		//Set<Node> NodesSelected = myHeuristic.select(prevLayer);
-		Set<Node> NodesSelected = prevLayer;
-		
-		// mapping each node to a list of nodes
-		HashMap<Node, LinkedList<Node>> nodeToList = new HashMap();
-		
-		// put selected nodes into the map
-		for(Node pNode : NodesSelected){
-			nodeToList.put(pNode, new LinkedList());
-		}
-		
-		while(ada.hasNext() && tmpNumIters-- > 0) {
-			
-			Model modelIteratedState = m.new ExactStateModel(ada.next());
-			
-			for(Node pNode : NodesSelected) {
-				// saved model that we compare to
-				try {
-					// if it's in this cluster, then we decluster 
-					if(pNode.dist(modelIteratedState) < EPSILON) { 
-						
-						boolean sameModel = false;
-						
-						if(!nodeToList.get(pNode).isEmpty()) {
-							ListIterator<Node> listIterator = nodeToList.get(pNode).listIterator();
-					        while (listIterator.hasNext()) {
-					            if(listIterator.next().dist(modelIteratedState) < EPSILON){
-					            	sameModel = true; // no need to create a new node
-					            	break;
-					            }
-					        }
+			while(ada.hasNext() && tmpNumIters-- > 0) {
+				//System.out.print(".");
+				//final Model modelIteratedState = new GroundedModel(m.new ExactStateModel(ada.next()), 100);
+				Model modelIteratedState = m.new ExactStateModel(ada.next());
+				//this will be true only if one of the newly created nodes 
+				//has the same model as the crt model we inspect
+				boolean sameModel = false; 
+				for(Node nFinalLayer : finalLayer) {
+					// saved model that we compare to
+					try {
+						if(nFinalLayer.dist(modelIteratedState) < EPSILON) { 
+							sameModel = true; 
+							break; 
 						}
-						
-						// if the associated list is empty 
-						// or the examining state not in any cluster
-						if(!sameModel){	
-							// create a new node
-							final Node nNew = new Node(nextIndex++, 
-									//new GroundedModel(modelIteratedState, 100),
-									modelIteratedState,
-									//ground the stateModel (so that integration can be solved faster)
-									new LInfComparator(m),
-									// TODO maybe use this - new LInfComparator(m), 
-									prevLayer, 
-									null /* done below*/);
-							nNew.activation = new MemoizedFeature() {
-								@Override
-								double eval_non_existent(State s) {
-									try {
-										return Math.exp(- ALPHA * nNew.dist(m.new ExactStateModel(s)));
-									} catch (IncorrectModelExpection e) {
-										e.printStackTrace();
-										return 0;
-									}
-								}
-							};
-							// add this new node to the list
-							nodeToList.get(pNode).add(nNew);
-						}	 
+					} catch (IncorrectModelExpection e) {
+						e.printStackTrace();
 					}
-				} catch (IncorrectModelExpection e) {
-					e.printStackTrace();
-				}
-			} // for nFinalLayer
-		}
+				} // for nFinalLayer
 
-		//construct the final layer
-		for(Entry<Node, LinkedList<Node>> n : nodeToList.entrySet()) {
-			if(n.getValue().isEmpty()){
-				finalLayer.add(n.getKey());
-				allNodes.add(n.getKey());
+				if(!sameModel) { //create a new node
+					final Node nNew = new Node(nextIndex++, 
+							//new GroundedModel(modelIteratedState, 100),
+							modelIteratedState,
+							//ground the stateModel (so that integration can be solved faster)
+							new LInfComparator(m),
+							// TODO maybe use this - new LInfComparator(m), 
+							prevLayer, 
+							null /* done below*/);
+					nNew.activation = new MemoizedFeature() {
+						@Override
+						double eval_non_existent(State s) {
+							try {
+								return Math.exp(- ALPHA * nNew.dist(m.new ExactStateModel(s)));
+							} catch (IncorrectModelExpection e) {
+								e.printStackTrace();
+								return 0;
+							}
+						}
+					};
+					finalLayer.add(nNew); // add it to result of declustering
+				}
 			}
-			else{
-				finalLayer.addAll(n.getValue());
-				allNodes.addAll(n.getValue());
+
+			// add new nodes to the final layer
+			for(Node n : finalLayer) {
+				allNodes.add(n);
 			}
 		}
 		
-		// copy the rest of the nodes which are not selected to the final layer
-		for(Node n: prevLayer){
-			if(!NodesSelected.contains(n)){
-				finalLayer.add(n);
-				allNodes.add(n);
+		// not the first layer
+		else{
+			// this will maintain the index of new nodes
+			int nextIndex = (allNodes == null) ? 0 : allNodes.size();
+	
+			// save the last layer from old graph - needed to test transition map
+			Set<Node> prevLayer = finalLayer;
+	
+			//we will rebuild the final layer from 0
+			finalLayer = new TreeSet<Node>();
+	
+			// get a fixed number of states to determine which nodes to create
+			Iterator<State> ada = m.get_state_iterator();
+			int tmpNumIters = numIters;
+			
+			// heuristic used to select a subset of nodes to analyze
+			Heuristic myHeuristic = new Heuristic(100, Heuristic.HeuristicType.RANDOM_PAIR);
+	
+			// select a subset of nodes to analyze
+			//Set<Node> NodesSelected = myHeuristic.select(prevLayer);
+			Set<Node> NodesSelected = prevLayer;
+			
+			// mapping each node to a list of nodes
+			HashMap<Node, LinkedList<Node>> nodeToList = new HashMap();
+			
+			// put selected nodes into the map
+			for(Node pNode : NodesSelected){
+				nodeToList.put(pNode, new LinkedList());
+			}
+			
+			while(ada.hasNext() && tmpNumIters-- > 0) {
+				
+				Model modelIteratedState = m.new ExactStateModel(ada.next());
+				
+				for(Node pNode : NodesSelected) {
+					// saved model that we compare to
+					try {
+						// if it's in this cluster, then we decluster 
+						if(pNode.dist(modelIteratedState) < EPSILON) { 
+							
+							boolean sameModel = false;
+							
+							if(!nodeToList.get(pNode).isEmpty()) {
+								ListIterator<Node> listIterator = nodeToList.get(pNode).listIterator();
+						        while (listIterator.hasNext()) {
+						            if(listIterator.next().dist(modelIteratedState) < EPSILON){
+						            	sameModel = true; // no need to create a new node
+						            	break;
+						            }
+						        }
+							}
+							
+							// if the associated list is empty 
+							// or the examining state not in any cluster
+							if(!sameModel){	
+								// create a new node
+								final Node nNew = new Node(nextIndex++, 
+										//new GroundedModel(modelIteratedState, 100),
+										modelIteratedState,
+										//ground the stateModel (so that integration can be solved faster)
+										new LInfComparator(m),
+										// TODO maybe use this - new LInfComparator(m), 
+										prevLayer, 
+										null /* done below*/);
+								nNew.activation = new MemoizedFeature() {
+									@Override
+									double eval_non_existent(State s) {
+										try {
+											return Math.exp(- ALPHA * nNew.dist(m.new ExactStateModel(s)));
+										} catch (IncorrectModelExpection e) {
+											e.printStackTrace();
+											return 0;
+										}
+									}
+								};
+								// add this new node to the list
+								nodeToList.get(pNode).add(nNew);
+							}	 
+						}
+					} catch (IncorrectModelExpection e) {
+						e.printStackTrace();
+					}
+				} // for nFinalLayer
+			}
+	
+			
+			//construct the final layer
+			for(Entry<Node, LinkedList<Node>> n : nodeToList.entrySet()) {
+				if(n.getValue().isEmpty()){
+					finalLayer.add(n.getKey());
+					allNodes.add(n.getKey());
+				}
+				else{
+					finalLayer.addAll(n.getValue());
+					allNodes.addAll(n.getValue());
+				}
+			}
+			
+			// copy the rest of the nodes which are not selected to the final layer
+			for(Node n: prevLayer){
+				if(!NodesSelected.contains(n)){
+					finalLayer.add(n);
+					allNodes.add(n);
+				}
 			}
 		}
 	}
